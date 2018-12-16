18459aa8144091b9016c92b625d1055dfb60c2a5
fangju <1342526004@qq.com>
2018-12-16 19:51:48 +0800

    登陆完成
    Signed-off-by: fangju <1342526004@qq.com>

---
+++ app/src/main/java/com/hasee/minibuslocalhost/util/SignalAnalysis.java
@@ -0,0 +1,12 @@
+package com.hasee.minibuslocalhost.util;
+
+import java.util.List;
+
+/**
+ * Created by fangju on 2018/12/16
+ * 信号解析，返回具体哪个屏幕的flag
+ */
+public class SignalAnalysis {
+    private List<String[]> signals;//信号集合
+
+}
