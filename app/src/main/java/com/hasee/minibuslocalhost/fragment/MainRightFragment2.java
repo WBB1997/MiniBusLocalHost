d15e9c32943753878262e72bf9288d85129ba859
fangju <1342526004@qq.com>
2018-12-17 11:26:12 +0800

    右边fragment基本完成
    Signed-off-by: fangju <1342526004@qq.com>

--- app/src/main/java/com/hasee/minibuslocalhost/fragment/MainRightFragment2.java
+++ app/src/main/java/com/hasee/minibuslocalhost/fragment/MainRightFragment2.java
@@ -32,7 +32,7 @@ public class MainRightFragment2 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_main_right_fragment2, container, false);
+        View view = inflater.inflate(R.layout.fragment_main_right2, container, false);
         return view;
     }
 
