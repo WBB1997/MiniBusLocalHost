4216b878d640a804aed4fa6dc3313879662dfbcd
fangju <1342526004@qq.com>
2018-12-17 22:42:34 +0800

    加入handler
    Signed-off-by: fangju <1342526004@qq.com>

--- app/src/main/java/com/hasee/minibuslocalhost/activity/MainActivity.java
+++ app/src/main/java/com/hasee/minibuslocalhost/activity/MainActivity.java
@@ -1,20 +1,58 @@
 package com.hasee.minibuslocalhost.activity;
 
+import android.content.Context;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.alibaba.fastjson.JSONObject;
 import com.hasee.minibuslocalhost.R;
+import com.hasee.minibuslocalhost.transmit.transmit;
 import com.hasee.minibuslocalhost.util.ActivityCollector;
+import com.hasee.minibuslocalhost.util.MyHandler;
 
 public class MainActivity extends BaseActivity {
-
+    private final int SEND_TO_FRONTSCREEN = 0;//前风挡
+    private final int SEND_TO_RIGHTSCREEN = 1;//右车门
+    private final int SEND_TO_LEFTSCREEN = 2;//左车门
+    private final int SEND_TO_LOCALHOST = 3;//主控屏
+    private Context mContext;//上下文
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mContext = MainActivity.this;
+        transmit.getInstance().setHandler(handler);
     }
 
     /**
+     * 接收CAN总线的信息，判断处理
+     */
+    private MyHandler handler = new MyHandler(mContext){
+        @Override
+        public void handleMessage(Message msg) {
+            JSONObject object = (JSONObject) msg.obj;//CAN总线的数据
+            switch (msg.what){
+                case SEND_TO_FRONTSCREEN:{//前风挡
+
+                    break;
+                }
+                case SEND_TO_RIGHTSCREEN:{//右车门
+                    break;
+                }
+                case SEND_TO_LEFTSCREEN:{//左车门
+                    break;
+                }
+                case SEND_TO_LOCALHOST:{//主控屏
+                    break;
+                }
+                default:
+                    break;
+            }
+        }
+    };
+
+  /**
      * 返回键监听
      */
     @Override
--- app/src/main/java/com/hasee/minibuslocalhost/util/SendToScreenThread.java
+++ app/src/main/java/com/hasee/minibuslocalhost/util/SendToScreenThread.java
@@ -1,5 +1,7 @@
 package com.hasee.minibuslocalhost.util;
 
+import com.alibaba.fastjson.JSONObject;
+
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
@@ -14,21 +16,22 @@ public class SendToScreenThread extends Thread {
     private DatagramSocket dSocket = null;
     private DatagramPacket dPacket = null;
     private byte[] buffer = null;//数据报大小
-    private String message = null;//接收的can总线信息
+    private JSONObject object = null;//接收的can总线信息
+    private int target = -1;//屏幕编号
 
-    public SendToScreenThread(String message){
-        this.message = message;
+    public SendToScreenThread(JSONObject object,int target){
+        this.object = object;
+        this.target = target;
     }
 
     @Override
     public void run() {
-        //处理can总线的信息
-        int flag = 1;
+        String message = object.toJSONString();
         buffer = message.getBytes();
         //发送给特定显示屏
         try {
             //根据命令号确定hostName
-            InetAddress address = InetAddress.getByName(hostNames[seekHostName(flag)]);
+            InetAddress address = InetAddress.getByName(hostNames[target]);
             dSocket = new DatagramSocket();
             dPacket = new DatagramPacket(buffer,buffer.length,address,port);
             dSocket.send(dPacket);
@@ -41,19 +44,4 @@ public class SendToScreenThread extends Thread {
         }
     }
 
-    /**
-     * 判断消息发送给哪个显示屏
-     * @param flag
-     * @return
-     */
-    public int seekHostName(int flag){
-        if(flag >= 1&& flag <= 3){//前风挡
-            return 0;
-        }else if(flag >= 11&& flag <= 14){//右车门
-            return 1;
-        }else if(flag >= 21&& flag <= 24){//左车门
-            return 2;
-        }
-        return 3;
-    }
 }
