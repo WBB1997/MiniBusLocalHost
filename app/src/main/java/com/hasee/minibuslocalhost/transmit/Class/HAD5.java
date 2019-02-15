4793676402e002b4987787e0ec4f0b4a62aa9b2b
wubeibei <690252189@qq.com>
2019-02-15 11:42:52 +0800

    2019年2月15日11:42:48

--- app/src/main/java/com/hasee/minibuslocalhost/transmit/Class/HAD5.java
+++ app/src/main/java/com/hasee/minibuslocalhost/transmit/Class/HAD5.java
@@ -13,13 +13,13 @@ import static com.hasee.minibuslocalhost.util.ByteUtil.*;
 public class HAD5 extends BaseClass {
     private static final String TAG = "HAD5";
     private HashMap<Integer, MyPair<Integer>> fields = new HashMap<Integer, MyPair<Integer>>(){{
-        put(0,new MyPair<>(8, IntegerCommand.HAD_CurrentDrivingRoadIDNum, MainActivity.SEND_TO_SCREEN)); // 档位位置;
-        put(8,new MyPair<>(8, IntegerCommand.HAD_NextStationIDNumb, MainActivity.SEND_TO_SCREEN)); // 档位位置;
-        put(24,new MyPair<>(16, IntegerCommand.HAD_DistanceForNextStation, MainActivity.SEND_TO_SCREEN)); // 档位位置;
-        put(32,new MyPair<>(8, IntegerCommand.HAD_TimeForArrivingNextStation, MainActivity.SEND_TO_SCREEN)); // 档位位置;
-        put(40,new MyPair<>(8, IntegerCommand.HAD_StartingSiteNum, MainActivity.SEND_TO_LOCALHOST)); // 档位位置;
-        put(48,new MyPair<>(8, IntegerCommand.HAD_EndingSiteNum, MainActivity.SEND_TO_LOCALHOST)); // 档位位置;
-        put(56,new MyPair<>(8, IntegerCommand.HAD_PassingBySiteNum, MainActivity.SEND_TO_LOCALHOST)); // 档位位置;
+        put(0,new MyPair<>(8, IntegerCommand.HAD_CurrentDrivingRoadIDNum, MainActivity.SEND_TO_SCREEN)); // 当前行驶线路ID;
+        put(8,new MyPair<>(8, IntegerCommand.HAD_NextStationIDNumb, MainActivity.SEND_TO_SCREEN)); // 下一个站点ID;
+        put(24,new MyPair<>(16, IntegerCommand.HAD_DistanceForNextStation, MainActivity.SEND_TO_SCREEN)); // 距离下一个站点的距离;
+        put(32,new MyPair<>(8, IntegerCommand.HAD_TimeForArrivingNextStation, MainActivity.SEND_TO_SCREEN)); // 预计到达下一个站点所需时间;
+        put(40,new MyPair<>(8, IntegerCommand.HAD_StartingSiteNum, MainActivity.SEND_TO_SCREEN)); // 起始站点ID;
+        put(48,new MyPair<>(8, IntegerCommand.HAD_EndingSiteNum, MainActivity.SEND_TO_SCREEN)); // 终点站点ID;
+        put(56,new MyPair<>(8, IntegerCommand.HAD_PassingBySiteNum, MainActivity.SEND_TO_SCREEN)); // 途径站点ID;
     }};
     private byte[] bytes = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 
